25f118b396c2abc04b22548737cd6e0a48c6b4ad
Sayan Basak <sayan.basak@outlook.com>
2022-05-17 01:28:53 +0530

    prints decimal to console

--- lib/main.dart
+++ lib/main.dart
@@ -2,82 +2,68 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(Application());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Application extends StatelessWidget {
+  TextEditingController _binaryController = TextEditingController();
+
+  void _returnDecimal() {
+    num decimal = 0;
+    num weight = 1;
+    num binary = num.parse(_binaryController.text);
+    num remainder;
+    print('Binary Value Transferred ${binary}');
+    while (binary != 0) {
+      remainder = binary % 10;
+      decimal = decimal + remainder * weight;
+      weight = weight * 2;
+      binary = binary / 10;
+    }
+    print('Decimal Value is ${binary}');
+    // print('Decimal Value is ${_binaryController.text}');
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Binary 2 Decimal',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const MyHomePage(title: 'Simple Binary to Decimal Value Converter'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  dynamic decimal = 0;
-  dynamic weight = 1;
-  dynamic binary = 1001;
-  dynamic remainder;
-
-  dynamic _returnDecimal() {
-    while (binary != 0) {
-      remainder = binary % 10;
-      decimal = decimal + remainder * weight;
-      binary = binary / 10;
-      weight = weight * 2;
-    }
-    print(_returnDecimal());
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              '1001 Converted to Decimal is:',
+      title: "Binary 2 Decimal",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Simple Binary to Decimal Converter"),
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Input Binary Value"),
+                Container(
+                  margin: const EdgeInsets.all(20.0),
+                  child: TextField(
+                    controller: _binaryController,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.allow(
+                        RegExp(r'[0-1]+'),
+                      ),
+                    ],
+                  ),
+                ),
+                // new ElevatedButton(
+                //   child: Text("Convert"),
+                //   onPressed: _returnDecimal,
+                // ),
+              ],
             ),
-            Container(
-              child: TextField(
-                keyboardType: TextInputType.number,
-                inputFormatters: [
-                  FilteringTextInputFormatter.allow(
-                    RegExp(r'[0-1]+'),
-                  )
-                ],
-              ),
-            ),
-            // Text(
-            //   '$_returnDecimal',
-            //   style: Theme.of(context).textTheme.headline4,
-            // ),
-          ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _returnDecimal,
+          tooltip: 'Convert',
+          child: const Icon(Icons.adjust_sharp),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _returnDecimal,
-        tooltip: 'Convert',
-        child: const Icon(Icons.adjust_sharp),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
